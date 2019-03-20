c7e703a43f939a3e3a78519bbe116fc36d8516e9
mesa <mehufarm@gmail.com>
2019-03-20 12:43:26 -0700

    done

--- webapp.py
+++ webapp.py
@@ -2,6 +2,7 @@ from flask import Flask, redirect, url_for, session, request, jsonify, Markup
 from flask import render_template
 from flask_mail import Mail, Message
 from time import localtime, strftime
+from werkzeug.utils import secure_filename
 import csv
 import pprint
 import os
@@ -19,6 +20,7 @@ app.config['MAIL_USERNAME'] = 'mehufarm@gmail.com'
 app.config['MAIL_PASSWORD'] = os.environ['MAIL_PASSWORD']
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
+app.config['UPLOAD_FOLDER']='/Users/P3/Desktop'
 mail=Mail(app)
 
 @app.route('/')
@@ -35,6 +37,9 @@ def Page3():
 @app.route('/Page4')
 def Page4():
     return render_template('Page4.html')
+@app.route('/Page5')
+def Page5():
+    return render_template('Page5.html')
 
 @app.route('/next1',methods=["POST","GET"])
 def rendernext1():
@@ -54,14 +59,30 @@ def rendernext3():
     session["data2"]=request.form["data"]
     return render_template('Page4.html')
 
+@app.route('/next4',methods=["POST","GET"])
+def rendernext4():
+    print(request.form)
+    session["data3"]=request.form["data"]
+    return render_template('Page5.html')
+
+@app.route('/next5',methods=["POST","GET"])
+def rendernext5():
+    print(request.form)
+    session["data4"]=request.form["data"]
+    return render_template('Page6.html')
 
 @app.route('/finish',methods=["POST","GET"])
 def renderfinish():
-
-    messg = 'Name: ' + session['data1'] + ',' + 'Smash Main: '+ session['data2'] + ',' +"Region: "+ str(request.form['data'] + ',' +'Time; '+ (strftime(" %d %b %Y %H:%M:%S", localtime())))
+    file=request.files['data']
+    #bytefile=os.fsencode(file)
+    #bytefile=open(file, "rb").read()
+    # print(type(file))
+    # file.save(app.config['UPLOAD_FOLDER'], "tmp.png")
+    messg = 'Name: ' + session['data1'] + ',' + 'Smash Main: '+ session['data2'] + ',' +"Name: "+ session["data3"] + ',' + "Name: "+ session["data4"] + ','+'Time; '+ (strftime("%d %b %Y %H:%M:%S", localtime()))
     msg = Message('User Dats', sender = 'mehufarm@gmail.com', recipients = ['mehufarm@gmail.com'])
-    msg.attach("data.csv", "data/csv" , messg )
+    msg.attach("data.csv", "text/csv" , messg )
 
+    msg.attach("image.png", "image/png", file.read())
     mail.send(msg)
     return render_template('Page1.html' , sent="Your survey is complete and your awnsers are sent.")
 
